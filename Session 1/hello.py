51152ef0260e01a90cac252448714e1a7c81d048
Allison-Fernandez <42825789+Allison-Fernandez@users.noreply.github.com>
2018-08-31 22:39:40 -0400

    Session 1 Homework
    Exercise 1

---
+++ Session 1/hello.py
@@ -0,0 +1,17 @@
+print ('Hello, Allison!')
+
+print ('Hello, World!'
+
+print 'Hello, World!'
+
+print ('Hello, World!)
+
+print (Hello, World!)
+
++3
+
+2++2
+
+02
+
+3 5
---
+++ calchw.py
@@ -0,0 +1,98 @@
+ 60*42+42
+     ^
+SyntaxError: invalid syntax
+>>> 2562
+2562
+>>> >>> 10/1.61
+  File "<stdin>", line 1
+    >>> 10/1.61
+     ^
+SyntaxError: invalid syntax
+>>> 6.211180124223602
+6.211180124223602
+>>> >>> 42/60
+  File "<stdin>", line 1
+    >>> 42/60
+     ^
+SyntaxError: invalid syntax
+>>> 0.7
+0.7
+>>> >>> 42.7/6.211180124223602
+  File "<stdin>", line 1
+    >>> 42.7/6.211180124223602
+     ^
+SyntaxError: invalid syntax
+>>> 6.874700000000002
+6.874700000000002
+>>> >>> //average pace in minutes
+  File "<stdin>", line 1
+    >>> //average pace in minutes
+     ^
+SyntaxError: invalid syntax
+>>>   File "<stdin>", line 1
+  File "<stdin>", line 1
+    File "<stdin>", line 1
+    ^
+IndentationError: unexpected indent
+>>>     //average pace in minutes
+  File "<stdin>", line 1
+    //average pace in minutes
+    ^
+IndentationError: unexpected indent
+>>>      ^
+  File "<stdin>", line 1
+    ^
+    ^
+IndentationError: unexpected indent
+>>> SyntaxError: invalid syntax
+  File "<stdin>", line 1
+    SyntaxError: invalid syntax
+                              ^
+SyntaxError: invalid syntax
+>>> >>> 2562/6.211180124223602
+  File "<stdin>", line 1
+    >>> 2562/6.211180124223602
+     ^
+SyntaxError: invalid syntax
+>>> 412.482
+412.482
+>>> >>> //average pace in seconds
+  File "<stdin>", line 1
+    >>> //average pace in seconds
+     ^
+SyntaxError: invalid syntax
+>>>   File "<stdin>", line 1
+  File "<stdin>", line 1
+    File "<stdin>", line 1
+    ^
+IndentationError: unexpected indent
+>>>     //average pace in seconds
+  File "<stdin>", line 1
+    //average pace in seconds
+    ^
+IndentationError: unexpected indent
+>>>      ^
+  File "<stdin>", line 1
+    ^
+    ^
+IndentationError: unexpected indent
+>>> SyntaxError: invalid syntax
+  File "<stdin>", line 1
+    SyntaxError: invalid syntax
+                              ^
+SyntaxError: invalid syntax
+>>> >>> 42.7/60
+  File "<stdin>", line 1
+    >>> 42.7/60
+     ^
+SyntaxError: invalid syntax
+>>> 0.7116666666666667
+0.7116666666666667
+>>> >>> 6.211180124223602/.7116666666666667
+  File "<stdin>", line 1
+    >>> 6.211180124223602/.7116666666666667
+     ^
+SyntaxError: invalid syntax
+>>> 8.727653570337614
+8.727653570337614
+>>>
